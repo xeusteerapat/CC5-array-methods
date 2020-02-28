103face5f4525ef8843b08fe931a10106e73d74e
xeusteerapat <xeus085@gmail.com>
2020-02-28 10:18:55 +0700

    refactor date function

--- MapAndSet.js
+++ MapAndSet.js
@@ -34,15 +34,15 @@ function unique(arr) {
 }
 
 let values = [
-  "Hare",
-  "Krishna",
-  "Hare",
-  "Krishna",
-  "Krishna",
-  "Krishna",
-  "Hare",
-  "Hare",
-  ":-O"
+  'Hare',
+  'Krishna',
+  'Hare',
+  'Krishna',
+  'Krishna',
+  'Krishna',
+  'Hare',
+  'Hare',
+  ':-O'
 ];
 
 console.log(unique(values)); // Hare, Krishna, :-O
--- mapArray.js
+++ mapArray.js
@@ -6,18 +6,18 @@ const nums3 = [1, 2, 3, 4];
 const nums4 = nums3.map(num => String(num));
 console.log(nums4);
 
-const arr5 = [1, "1", 2, {}];
+const arr5 = [1, '1', 2, {}];
 const arr6 = arr5.map(num => typeof num);
 console.log(arr6);
 
-const arr7 = ["apple", "banana", "orange"];
+const arr7 = ['apple', 'banana', 'orange'];
 const arr8 = arr7.map(word => word.toUpperCase());
 console.log(arr8);
 
 const arr9 = [
-  { name: "apple", age: 14 },
-  { name: "banana", age: 18 },
-  { name: "watermelon", age: 32 }
+  { name: 'apple', age: 14 },
+  { name: 'banana', age: 18 },
+  { name: 'watermelon', age: 32 }
 ];
 
 const arr10 = arr9.map(name => name.name);
@@ -26,9 +26,9 @@ const arr11 = arr9.map(name => name.age);
 console.log(arr11);
 
 const array12 = [
-  { name: "apple", surname: "London" },
-  { name: "banana", surname: "Bangkok" },
-  { name: "watermelon", surname: "Singapore" }
+  { name: 'apple', surname: 'London' },
+  { name: 'banana', surname: 'Bangkok' },
+  { name: 'watermelon', surname: 'Singapore' }
 ];
 
 const arr13 = array12.map(item => `${item.name} ${item.surname}`);
@@ -36,8 +36,8 @@ console.log(arr13);
 
 const arr14 = [1, 3, 4, 5, 6, 7, 8];
 const arr15 = arr14.map(item => {
-  if (item % 2 === 0) return "even";
-  else return "odd";
+  if (item % 2 === 0) return 'even';
+  else return 'odd';
 });
 
 console.log(arr15);
@@ -55,13 +55,13 @@ const arr19 = arr18.map(item => {
 console.log(arr19);
 
 const arr20 = [
-  { name: "apple", birth: "2000-01-01" },
-  { name: "banana", birth: "1990-10-01" },
-  { name: "watermelon", birth: "1985-12-01" }
+  { name: 'apple', birth: '2000-01-01' },
+  { name: 'banana', birth: '1990-10-01' },
+  { name: 'watermelon', birth: '1985-12-01' }
 ];
 
 const arr21 = arr20.map(obj => {
-  let now = new Date("2019-11-01").getTime();
+  let now = new Date('2019-11-01').getTime();
   let birthDate = new Date(obj.birth).getTime();
   let age = Math.floor((now - birthDate) * 3.17098e-11);
 
@@ -84,18 +84,15 @@ console.log(arr21);
 // console.log(arr21);
 
 const arr22 = [
-  { name: "apple", birth: "2000-01-01" },
-  { name: "banana", birth: "1990-10-10" },
-  { name: "watermelon", birth: "1985-12-30" }
+  { name: 'apple', birth: '2000-01-01' },
+  { name: 'banana', birth: '1990-10-10' },
+  { name: 'watermelon', birth: '1985-12-30' }
 ];
 
 const arr23 = arr22.map(obj => {
-  let date = new Date(obj.birth)
-    .toString()
-    .slice(4)
-    .split(" ");
+  let date = new Date(obj.birth).toString().split(' ');
   return `
-  <tr><td>${obj.name}</td><td>${date[1]} ${date[0]} ${date[2]}</td></tr>
+  <tr><td>${obj.name}</td><td>${date[2]} ${date[1]} ${date[3]}</td></tr>
   `;
 });
 
